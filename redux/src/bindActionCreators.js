b7c69bd856e117fb3b1e61b81f7a927a89b89016
jyzwf <1327409246@qq.com>
2018-01-31 15:46:18 +0800

    add bindActionCreators.js comment

--- redux/src/bindActionCreators.js
+++ redux/src/bindActionCreators.js
@@ -4,6 +4,59 @@ function bindActionCreator(actionCreator, dispatch) {
     return (...args) => dispatch(actionCreator(...args))
 }
 
+/**
+ * 结合 react-redux 中有如下用法：
+ * function mapDispatchToProps(dispatch) {
+ *      return bindActionCreators(CounterActions, dispatch)
+ * }
+ * 
+ * 这里的 CounterActions 有很多形式，如：
+ *1.  export const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
+ * 
+ * 
+ *2.    export function increment() {
+	        return {
+		        type: INCREMENT_COUNTER
+	        }
+        }
+
+
+  3.    export function incrementIfOdd() {
+	        return (dispatch, getState) => { // 这里 dispatch 一个函数时，就可能要用到 redux-thunk 模块等，因为直接dispath 函数会报错
+		    //获取state对象中的counter属性值
+		        const {
+			        counter
+		        } = getState()
+
+		        //偶数则返回
+		        if (counter % 2 === 0) {
+			        return
+		        }
+		        //没有返回就执行加一
+		        dispatch(increment())
+	        }
+        }
+
+
+    此时 actionCreators 就是一个对象，
+    actionCreators={
+        ... 
+    }
+
+    bindActionCreators期望收到的是函数或者是对象，但是对象里面的每个键值都是函数，，否则会警告
+
+
+    最终返回 boundAcionCreators，以上面为例，结果如下：
+
+    boundAcionCreators={
+
+        increment:(...args) => dispatch(increment(...args)),
+
+        
+        incrementIfOdd:(...args) => dispatch(incrementIfOdd(...args)),
+    }
+ * 
+ */
 export default function bindActionCreators(actionCreators, dispatch) {
     if (typeof actionCreators === 'function') {
         return bindActionCreator(actionCreators, dispatch)
@@ -16,6 +69,7 @@ export default function bindActionCreators(actionCreators, dispatch) {
         )
     }
 
+    // 遍历 actionCreators 对象
     const keys = Object.keys(actionCreators)
     const boundAcionCreators = {}
     for (let i = 0; i < keys.length; i++) {
